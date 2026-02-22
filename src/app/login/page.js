7ac3db5e0cc751667bc82a6f83dc55faa77281aa
AEbdoRohman <aebdolrohman@gmail.com>
2026-02-23 00:43:12 +0200

    update login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { Input } from "@components/form-filed";
+import Input from "@/components/ui/input";
 
 export default function SigninPage() {
   const router = useRouter();
